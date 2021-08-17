8462845c4768a509f9c8921445a669d097cef4f9
rapPayne <bob@creator.net>
2021-08-17 15:46:18 -0500

    Platform detection for emulated devices

--- starters/state.dart
+++ starters/state.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
@@ -11,11 +13,20 @@ final selectedFilmProvider = StateProvider<Film?>((_) => null);
 final showingsProvider = StateProvider((_) => <dynamic>[]);
 final showFilmDetailsProvider = StateProvider((_) => false);
 
-String _baseUrl = "http://localhost:3007";
+String getBaseUrl({String port = "3007"}) {
+  // android 10.0.2.2 via AVD
+  // android 10.0.3.2 via Genymotion
+  // tethered devices will need your actual IP address.
+  String _baseUrl = "http://127.0.0.1:$port";
+  if (Platform.isAndroid)
+    return "http://10.0.2.2:$port";
+  else
+    return _baseUrl;
+}
 
 // Fetch films using a strongly-typed class
 Future<List<Film>> fetchFilms() {
-  String url = "$_baseUrl/api/films";
+  String url = "${getBaseUrl()}/api/films";
   return http.get(Uri.parse(url)).then((res) {
     List<dynamic> films = jsonDecode(res.body);
     return films.map((f) => Film.fromJson(f)).toList();
@@ -24,7 +35,7 @@ Future<List<Film>> fetchFilms() {
 
 // Fetch one film by id.
 Future<Film> fetchFilm({required int id}) {
-  String url = "http://localhost:3007/api/films/$id";
+  String url = "${getBaseUrl()}/api/films/$id";
   return http.get(Uri.parse(url)).then((res) {
     return Film.fromJson(jsonDecode(res.body));
   });
@@ -33,6 +44,6 @@ Future<Film> fetchFilm({required int id}) {
 // Fetch showings using a dynamic Map
 Future<dynamic> fetchShowings({required int film_id, required DateTime date}) {
   String url =
-      "$_baseUrl/api/showings/$film_id/${DateFormat('yyyy-MM-dd').format(date)}";
+      "${getBaseUrl()}/api/showings/$film_id/${DateFormat('yyyy-MM-dd').format(date)}";
   return http.get(Uri.parse(url)).then((res) => jsonDecode(res.body));
 }
