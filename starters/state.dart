0bc5f16591e8e1c4132bf5e663e4f26ffce729bc
rapPayne <bob@creator.net>
2021-08-20 08:17:00 -0500

    Updated state.dart

--- starters/state.dart
+++ starters/state.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
-
+import 'dart:math';
+import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
@@ -16,8 +17,9 @@ final showFilmDetailsProvider = StateProvider((_) => false);
 String getBaseUrl({String port = "3007"}) {
   // android 10.0.2.2 via AVD
   // android 10.0.3.2 via Genymotion
-  // tethered devices will need your actual IP address.
   String _baseUrl = "http://127.0.0.1:$port";
+  if (kIsWeb)
+    return _baseUrl; // Must be first b/c Platform.operatingSystem throws on web.
   if (Platform.isAndroid)
     return "http://10.0.2.2:$port";
   else
@@ -46,4 +48,40 @@ Future<dynamic> fetchShowings({required int film_id, required DateTime date}) {
   String url =
       "${getBaseUrl()}/api/showings/$film_id/${DateFormat('yyyy-MM-dd').format(date)}";
   return http.get(Uri.parse(url)).then((res) => jsonDecode(res.body));
+}
+
+/*
+TODO: This logic should be done on the server for security reasons.
+Move it there eventually. Add a firstName, lastName, etc to the 
+reservation. Also, the user should be logged in at 
+this point so get their int user_id also.
+*/
+// Make a purchase
+Future<dynamic> buyTickets({required Map<String, dynamic> purchase}) async {
+  // String? firstName,
+  // String? lastName,
+  // String? email,
+  // String? phone,
+  // String? creditCardNumber,
+  // String? CVV,
+  // String? expiryMonth,
+  // String? expiryYear,
+  // required int showing_id,
+  // If these two don't exist, it'll throw and be caught on the outside.
+  List<int> seats = purchase["seats"];
+  int showing_id = purchase["showing_id"];
+
+  String url = "${getBaseUrl()}/api/reservations";
+  List<Future<dynamic>> _futures = [];
+  final _payment_key = "temp_${Random().nextInt(2140000000)}";
+  for (int seat_id in seats) {
+    Map<String, dynamic> _body = {
+      "showing_id": showing_id.toString(),
+      "seat_id": seat_id.toString(),
+      "user_id": 1.toString(),
+      "payment_key": _payment_key,
+    };
+    _futures.add(http.post(Uri.parse(url), body: _body));
+  }
+  return Future.wait(_futures);
 }
