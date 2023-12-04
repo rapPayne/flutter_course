17bd83c5436ecde942d7f7e8fec2e636e8e04f4a
rapPayne <bob@creator.net>
2023-12-04 16:07:15 -0600

    Minor refinements

--- examples/giphy_app/lib/main.dart
+++ examples/giphy_app/lib/main.dart
@@ -1,11 +1,8 @@
 // ignore_for_file: avoid_print
 
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:giphy_app/draw_random_gif.dart';
-import 'package:http/http.dart';
 
 void main() async {
   await dotenv.load(fileName: ".env");
@@ -21,7 +18,7 @@ class MyApp extends StatelessWidget {
         title: 'Random Giphy Viewer',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          colorScheme: ColorScheme.light().copyWith(primary: Colors.red),
         ),
         home: const DrawRandomGif());
   }
