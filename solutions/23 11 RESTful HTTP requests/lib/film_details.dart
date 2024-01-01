c546190fdf51eaffcdf5c9a86e44a99ba020f62e
rapPayne <bob@creator.net>
2024-01-01 09:00:15 -0600

    Lab 23 solution RESTful requests

---
+++ solutions/23 11 RESTful HTTP requests/lib/film_details.dart
@@ -0,0 +1,42 @@
+import 'package:daam/showing_times.dart';
+import 'package:daam/state/film.dart';
+import 'package:daam/state/global.dart';
+import 'package:flutter/material.dart';
+
+class FilmDetails extends StatelessWidget {
+  const FilmDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Film selectedFilm = global.get<Film>("selectedFilm");
+    DateTime selectedDate = global.get<DateTime>("selectedDate");
+
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          const Text("FilmDetails"),
+          Image.network("http://localhost:3008/${selectedFilm.posterPath}"),
+          ShowingTimes(
+            film: selectedFilm,
+            selectedDate: selectedDate,
+          ),
+          Text(
+            selectedFilm.title,
+          ),
+          Text(
+            selectedFilm.tagline ?? "",
+          ),
+          Text(
+            selectedFilm.homepage ?? "",
+          ),
+          Text(
+            selectedFilm.overview ?? "",
+          ),
+          Text(
+            "Rating: ${selectedFilm.voteAverage}/10 ${selectedFilm.voteCount} votes",
+          ),
+        ],
+      ),
+    );
+  }
+}
